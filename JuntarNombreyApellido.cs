6139f79b426a624ac8c20ac0345a58d4ec6629d6
DermisZabala <dermiszabala@gmail.com>
2024-08-24 23:14:02 -0400

    Utilizando INotifyPropertyChanged

---
+++ JuntarNombreyApellido.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_INotifyPropertyChanged
+{
+    public class JuntarNombreyApellido : INotifyPropertyChanged
+    {
+        //Campos de clase creados
+        private string nombre, apellido, nombre_apellido;
+
+        //Metodo que implementa la interfaz INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /*Propiedad Nombre que almacena valor en textbox
+         Y que completa el textBox Nombre y apellido gracias al metodo
+         creado void MiNotificacion*/
+        public string Nombre
+        {
+            get { return nombre;}
+            set 
+            {
+                nombre = value; 
+                MiNotificacion("NombreApellido"); 
+            }
+        }
+
+        /*Propiedad Apellido que almacena valor en textbox
+         Y que completa el textBox Nombre y apellido gracias al metodo
+         creado void MiNotificacion*/
+        public string Apellido
+        {
+            get { return apellido; }
+            set 
+            {
+                apellido = value;
+                MiNotificacion("NombreApellido");
+            }
+        }
+
+
+        /*
+          Propiedad Nombre que almacena el valor de las propiedades 
+          Nombre, Apellido en textbox y modifica el valor de los texbox Nombre, Apellido
+          TwoWay 
+        */
+        public string NombreApellido
+        {
+            get
+            {
+                nombre_apellido = Nombre + " " + Apellido;
+                return nombre_apellido;
+                
+            }
+            set 
+            { 
+                nombre = value;
+                apellido = value;
+                MiNotificacion("Nombre");
+                MiNotificacion2("Apellido");
+            }
+        }
+
+        //Metodo que pasa por parametro una propiedad para poder
+        //hacer los cambios desde el textbox en tiempo de ejecucion
+        public void MiNotificacion(string Property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(Property));
+            }
+            
+        }
+        public void MiNotificacion2(string apellido)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(apellido));
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_INotifyPropertyChanged
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Pasandole por parametro valores a las propiedades 'Nombre', 'Apellido'
+            juntarNombreApellido = new JuntarNombreyApellido { Nombre = "Hamel", Apellido = "Zabala"};
+            
+            //Pasandole los datos de las propiedades (Nombre, Apellido) a los TextBox
+            this.DataContext = juntarNombreApellido;
+        }
+
+        //Creacion del objeto 'juntarNombreApellido'
+        public JuntarNombreyApellido juntarNombreApellido;
+        
+    }
+}
